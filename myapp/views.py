b7d8a4ba1c4061dd38b2b08777ae3b6a73340092
Krishan Dalal <krishandalal024@gmail.com>
2022-01-11 16:52:08 +0530

    changes in index.html

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 
 def index(request):
-    return render(request,'index.html',)
+    return render(request,'index.html')
 
 def about(request):
     return HttpResponse("about")
